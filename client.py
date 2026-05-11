b66846232622f8240671299b85c71498c2642ece
Samuel0508 <92311541+samuel3050@users.noreply.github.com>
2026-05-11 12:31:26 +0200

    client url fix

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import time
 
 def join_game():
     # Öffne den Browser zur Lobby
-    url = 'http://10.38.80.95:5000/lobby'
+    url = 'http://10.38.80.17:5000/lobby'
     print(f"Öffne Browser zur Lobby: {url}")
     webbrowser.open(url)
     print("Browser geöffnet. Gib deinen Namen in der Lobby ein und warte auf andere Spieler!")
